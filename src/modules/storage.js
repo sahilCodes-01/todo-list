f56b092b4f9c1a1529fec80459d35504ec2035f2
sahil-dev <232035734+sahilCodes-01@users.noreply.github.com>
2026-07-21 05:42:29 +0000

    add Storage module with save and load functions for localStorage persistence

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,19 +1,45 @@
-const save = (projects) => {
+import Project from "./project";
+import Todo from "./todo";
 
+const save = (projects) => {
   localStorage.setItem("projects", JSON.stringify(projects));
 };
 
 const load = () => {
- 
   const data = localStorage.getItem("projects");
-  
+
   if (data === null) {
     return [];
   } 
   
   else {
+    const parsedData = JSON.parse(data);
 
-    return JSON.parse(data);
+    const rebuilt = parsedData.map((plainProject) => {
+
+      const newProject = new Project(plainProject.name);
+
+      newProject.id = plainProject.id;
+
+      plainProject.todos.forEach((plainTodo) => {
+        
+
+        const newTodo = new Todo(
+          plainTodo.title,
+          plainTodo.description,
+          plainTodo.dueDate,
+          plainTodo.priority,
+          plainTodo.notes,
+          plainTodo.done,
+        );
+        newTodo.id = plainTodo.id;
+        newProject.addTodo(newTodo);
+      });
+
+      return newProject;
+    });
+
+    return rebuilt;
   }
 };
 
